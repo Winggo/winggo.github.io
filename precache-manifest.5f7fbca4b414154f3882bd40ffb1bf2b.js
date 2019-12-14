3c66b6f08ffd1ca6b38c0c1d6c100de77919ef65
winggo <winggo.tse@gmail.com>
2019-12-14 02:10:52 -0800

    Updates

---
+++ precache-manifest.5f7fbca4b414154f3882bd40ffb1bf2b.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b06d5bcbadf776ed9946ef990749158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc72fdc65c1cc473c9bf",
+    "url": "/static/css/main.2f9e7bb8.chunk.css"
+  },
+  {
+    "revision": "553ede4d33d6ae04950a",
+    "url": "/static/js/2.b77a061e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.b77a061e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc72fdc65c1cc473c9bf",
+    "url": "/static/js/main.94d6d413.chunk.js"
+  },
+  {
+    "revision": "18b07e86edab6e87a93d",
+    "url": "/static/js/runtime-main.0286bf45.js"
+  },
+  {
+    "revision": "24b6c5c3725bed05dfc2216c49f9571e",
+    "url": "/static/media/AboveItAll.24b6c5c3.jpg"
+  },
+  {
+    "revision": "0992d9dad33d524fee86c346e0b9f0ee",
+    "url": "/static/media/IMG_0383.0992d9da.jpg"
+  },
+  {
+    "revision": "b0b0c1646c1aff929dcfbf7ea282ade0",
+    "url": "/static/media/IMG_5621.b0b0c164.jpg"
+  },
+  {
+    "revision": "955bb32f75d8edb85bb18be8b410fcff",
+    "url": "/static/media/P1201189.955bb32f.jpg"
+  },
+  {
+    "revision": "382744af13d21c89b2f5f7bfec1fb772",
+    "url": "/static/media/P1201200.382744af.jpg"
+  },
+  {
+    "revision": "4cce1126dcd5b24e667bca48b531463d",
+    "url": "/static/media/P1201235.4cce1126.jpg"
+  },
+  {
+    "revision": "adf0271f83f88cc29f57709ca8f03551",
+    "url": "/static/media/P1201280.adf0271f.jpg"
+  },
+  {
+    "revision": "cef76372af45e7597fec05780247f8b3",
+    "url": "/static/media/P1201305.cef76372.jpg"
+  },
+  {
+    "revision": "1d0b9d1f820cfceb31f74ece04453a19",
+    "url": "/static/media/P1211360.1d0b9d1f.jpg"
+  },
+  {
+    "revision": "20b5dda5406712f228c4539300e92bd7",
+    "url": "/static/media/P1211409.20b5dda5.jpg"
+  },
+  {
+    "revision": "dbc856cf760e5ab8ab67cb8a4b5b1385",
+    "url": "/static/media/P1211503.dbc856cf.jpg"
+  },
+  {
+    "revision": "ed884d99e44a2933d5ce6d1ab7648357",
+    "url": "/static/media/P1211627.ed884d99.jpg"
+  },
+  {
+    "revision": "7691df9c5318a16032310d9a3ce7d236",
+    "url": "/static/media/P2184275.7691df9c.jpg"
+  },
+  {
+    "revision": "09ec6e39d11105fa70ec40c6812e750b",
+    "url": "/static/media/P7080819.09ec6e39.jpg"
+  },
+  {
+    "revision": "9b99759b5d8f3bdf3e192770e357a447",
+    "url": "/static/media/P7191303.9b99759b.jpg"
+  },
+  {
+    "revision": "26a17dd23b4f98ca653ad0dfe053665b",
+    "url": "/static/media/PC033026.26a17dd2.jpg"
+  },
+  {
+    "revision": "301c142249597d1c46fe1a7c794a354c",
+    "url": "/static/media/PC050865.301c1422.jpg"
+  },
+  {
+    "revision": "e75fe42cffa6bb204c50bf2c78d4e70a",
+    "url": "/static/media/WinggoTse_Resume.e75fe42c.pdf"
+  },
+  {
+    "revision": "43944fdb3b13c92e20306dcffe2558c3",
+    "url": "/static/media/Yosemite.43944fdb.mp4"
+  },
+  {
+    "revision": "c47289067a34cf72d22f3dde2856012a",
+    "url": "/static/media/YosemiteFalls.c4728906.jpg"
+  },
+  {
+    "revision": "12c56509cbfa161d054ad8fc5a1a25ba",
+    "url": "/static/media/YosemitePoster.12c56509.png"
+  },
+  {
+    "revision": "75c1c9a490009e624513d6ad5d5eb89b",
+    "url": "/static/media/clientSideRender.75c1c9a4.gif"
+  },
+  {
+    "revision": "6ffeca2630a8daad30b28f964d19eeaf",
+    "url": "/static/media/foodgo.6ffeca26.png"
+  },
+  {
+    "revision": "a71bd31f1b34ad0909c840c6c34041dc",
+    "url": "/static/media/glorify.a71bd31f.png"
+  },
+  {
+    "revision": "a3477d8dfd66359c5fb598b103815deb",
+    "url": "/static/media/newsletter.a3477d8d.png"
+  },
+  {
+    "revision": "32d54c88f06c03a0207e0e55398b68b3",
+    "url": "/static/media/portfolio.32d54c88.png"
+  },
+  {
+    "revision": "85424c158aa4f27dee0138ad8136ae79",
+    "url": "/static/media/serverSideRender.85424c15.gif"
+  },
+  {
+    "revision": "2dac54614052df407349db95bcc4238e",
+    "url": "/static/media/softprofiles.2dac5461.png"
+  }
+]);
